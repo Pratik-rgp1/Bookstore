37902ecc765eca28617e1a04bb348a4c7fc15d71
pratik-rgp1 <pratikregmi989@gmail.com>
2025-01-06 09:43:02 +0545

    Add Category table migration and related changes

---
+++ BookstoreWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿//This class is a basic class that has to be configured in this way in order to use ERC
+using BookstoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreWeb.Data
+{
+    public class ApplicationDbContext:DbContext  //DbCongtext is the root class that is used for EF(building class inside ERC inside nuget packages)
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories  { get; set; }
+    }
+}
---
+++ BookstoreWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BookstoreWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^\d+$", ErrorMessage = "DisplayOrder must be numeric.")]
+        public string DisplayOrder { get; set; }
+    }
+}
